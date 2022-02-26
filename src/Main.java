8dce5fe5f3a6d2dd9cad94c4233165a62f3ea3d4
Александр Сальников <alexander211981@mail.ru>
2022-02-26 14:13:06 +0700

    решение задачи 1.2 бонусы

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 100; // сумма на счете
+        int add = 1100; // сумма пополнения
+        int bonus = add / 100; // сумма бонуса
+        int sum = add > 1000 ? start + add + bonus : start + add; // расчет итоговой суммы
+        System.out.println(sum + " итоговая сумма");
+    }
+}
